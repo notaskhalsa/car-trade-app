6c072380194ce5f2ca2f6494949f9cf352896bc0
askhalsa <arshdeep.singh@crownstack.com>
2022-02-01 15:26:06 +0530

    Sqlite Error

--- car-trade-next-app/src/pages/_document.tsx
+++ car-trade-next-app/src/pages/_document.tsx
@@ -1,25 +1,22 @@
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript, Html } from 'next/document';
 import React from 'react';
 import { theme } from './_app';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+          <meta name="theme-color" content={theme.palette.primary.main} /> 
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>  
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
